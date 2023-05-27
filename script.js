803b2d9e22459bbf7ad4e9d32ae3343a9de23003
olgaodc <olgaodincova@gmail.com>
2023-05-27 20:17:57 +0300

    fetches changed

--- script.js
+++ script.js
@@ -1,17 +1,97 @@
-const postProject = (newProject) => {
-    fetch('https://642c65dc208dfe25472f319b.mockapi.io/projects', {
-        method: 'POST',
-        headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newProject),
-}) .then((data) => {
-    return data.json();
-}) .catch((err) => {
-    return err;
-});
+const displayItems = (projectItem) => {
+    const projectsContainer = document.querySelector('.projectsContainer');
+    const projectSection = document.createElement('div');
+    projectSection.setAttribute('class', 'projectSection');
+    projectsContainer.append(projectSection);
+
+    const name  = projectItem.projectName;
+
+    const deadline = projectItem.deadline;
+
+
+    const projectInfo = document.createElement('div');
+    projectInfo.setAttribute('class', 'projectInfo');
+    projectInfo.innerHTML = `There are a working plan for your project ${name}, your deadline is ${deadline}:`
+    projectSection.append(projectInfo);
+
+    const titles = document.createElement('div');
+    titles.setAttribute('class', 'titles');
+    projectSection.append(titles);
+
+    const dateTitle = document.createElement('span');
+    dateTitle.innerHTML = 'Date';
+    titles.append(dateTitle);
+
+    const hoursTitle = document.createElement('span');
+    hoursTitle.innerHTML = 'Hours';
+    titles.append(hoursTitle);
+
+
+    const busynessData = projectItem.busynessData;
+    busynessData.forEach(entry => {
+        const planItem = document.createElement('div');
+        planItem.setAttribute('class', 'planItem');
+        projectSection.append(planItem);
+
+        const date = document.createElement('span');
+        date.innerHTML = entry.date;
+        planItem.append(date);
+    
+        // const busyHours = document.createElement('span');
+        // busyHours.innerHTML = entry.busyHours;
+        // projectSection.append(busyHours);
+    
+        const workHours = document.createElement('span');
+        workHours.innerHTML = entry.workHours;
+        planItem.append(workHours);
+    });
 }
+
+
+const displayProjects = async () => {
+    try {
+        const response = await fetch('https://642c65dc208dfe25472f319b.mockapi.io/projects');
+        const data = await response.json();
+
+        data.sort((a, b) => {
+            return Date(a.date) - Date(a.date);
+        });
+
+        const projectsContainer = document.querySelector('.projectsContainer');
+        projectsContainer.innerHTML = ''; 
+
+        data.forEach(projectItem => {
+            return displayItems(projectItem);
+        })
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+
+
+const postProject = async (newProject) => {
+    try {
+        const response = await fetch('https://642c65dc208dfe25472f319b.mockapi.io/projects', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newProject),
+        });
+
+        if (!response.ok) {
+        throw new Error('Failed to post project');
+        }
+   
+        displayProjects();
+
+    } catch (err) {
+    console.log(err);
+    }
+};
 
 
 
@@ -35,7 +115,7 @@ button.addEventListener("click", () => {
 const submit = document.getElementById('submit');
 let message;
 
-submit.addEventListener('click', () => {    
+submit.addEventListener('click', async () => {    
     const projectName = document.getElementById('projectName').value;
     const projectHours = document.getElementById('projectHours').value;
     const deadline = document.getElementById('deadline').value;
@@ -49,6 +129,7 @@ submit.addEventListener('click', () => {
     };
     
     message = document.createElement('div');
+    message.setAttribute('class', 'message');
     message.style.color = "red";
     const form = document.querySelector('.form');
     form.append(message);
