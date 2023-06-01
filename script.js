02840091789b80d2005e72a28b86ebeed7bc1536
olgaodc <olgaodincova@gmail.com>
2023-06-01 15:31:49 +0300

    some errors added

--- script.js
+++ script.js
@@ -205,31 +205,63 @@ createButton.addEventListener('click', async () => {
     const form = document.querySelector('.form');
     form.append(message);
 
+    // projectHours whole number validation
+    const projectHoursInput = document.getElementById('projectHours');
+    const value = projectHoursInput.value;
+    const isValid = Number.isInteger(Number(value));
+
+    if (!isValid) {
+    return message.innerHTML= 'Please enter a whole number.';
+    }
+
+    // projectHours number validation
     if(projectHours < 1) {
-      return message.innerHTML = "Project Hours cannot be less than 1";
+      return message.innerHTML = "Project hours cannot be less than 1";
     }
-
     if(projectHours > 300) {
-        return message.innerHTML = "Project Hours cannot be more than 300";
+        return message.innerHTML = "Project hours cannot be more than 300";
     }
 
+    //All fields validation
     if (!projectName || !projectHours || !deadline || hasEmptyInputs(dateInputs) || hasEmptyInputs(busyHoursInputs)) {
         return message.innerHTML = 'Fill out all fields!!!!';
     };
 
+    //dates validation
     const today = new Date();
+    today.setHours(0, 0, 0, 0);
     const deadlineDate = new Date(deadline);
+    deadlineDate.setHours(0, 0, 0, 0);
     const totalDays = Math.ceil((deadlineDate - today) / (1000 * 60 * 60 * 24));
 
     if (deadlineDate < today) {
         return message.innerHTML = 'The deadline cannot be a past date';
     }
 
+    const date = document.querySelector('.date');
+    const selectedDate = new Date(date.value);
+    selectedDate.setHours(0, 0, 0, 0);
+    if (selectedDate < today) {
+        return message.innerHTML = 'The date cannot be a past date';
+    }
+
+    if (deadlineDate.getTime() === today.getTime()) {
+        return message.innerHTML = 'The deadline cannot be today';
+    }
+
+   
+    //busyHours calculation
     let totalBusyHours = 0;
 
     for (let i = 0; i < busyHoursInputs.length; i++) {
         const busyHoursInput = busyHoursInputs[i];
         const busyHours = Number(busyHoursInput.value);
+        const isValid = Number.isInteger(Number(busyHours));
+    
+        if (!isValid) {
+        return message.innerHTML= 'Please enter a whole number.';
+        };
+
         totalBusyHours += busyHours;
     }
 
@@ -243,9 +275,7 @@ createButton.addEventListener('click', async () => {
         return message.innerHTML = 'Added availability is after the project deadline';
     };
 
-
     const workHoursPerDay = projectHours / totalDays;
-
     const selectedDates = new Set();
 
     for (let i = 0; i < dateInputs.length; i++) {
